41bc928bf28eb46ec7f17cc395894113b9ea65c1
Elena Bravo <hey@elenabravo.com>
2022-01-21 00:20:05 +0100

    fix mobile menu

--- src/js/app.js
+++ src/js/app.js
@@ -31,7 +31,6 @@ document.onreadystatechange = function () {
 
     setTimeout(() => {
       body.classList.remove(isLoadingClassName)
-      menuActions()
       animateBgColorOnScroll()
       scrollTriggerAnimations()
       ghostAnimation()

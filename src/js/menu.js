c936d26368c9fa4111bb72eb0b251ed7c2bc069c
Elena Bravo <hey@elenabravo.com>
2022-01-22 17:54:48 +0100

    improve mobile menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,22 +2,14 @@ export default function mobileActions() {
   const { body } = document
 
   // mobile menu
-  const mobileMenuBtn = document.querySelector('.mobile-menu__button')
-  const mobileMenuBtnLabel = document.querySelector(
-    '.mobile-menu__button--label'
-  )
-  const labels = ['Close', 'Menu']
+  const mobileMenuBtns = document.querySelectorAll('.mobile-menu__button')
   const openClassName = 'menu-is-open'
 
-  mobileMenuBtn.addEventListener('click', function () {
-    // toggle button label
-    const mobileMenuBtnLabelText = mobileMenuBtnLabel.textContent
-    mobileMenuBtnLabel.textContent = labels.find(
-      (label) => label !== mobileMenuBtnLabelText
-    )
-
-    // toggle body className
-    body.classList.toggle(openClassName)
+  mobileMenuBtns.forEach((mobileMenuBtn) => {
+    mobileMenuBtn.addEventListener('click', function () {
+      // toggle body className
+      body.classList.toggle(openClassName)
+    })
   })
 
   // remove open class from body when menu item is clicked

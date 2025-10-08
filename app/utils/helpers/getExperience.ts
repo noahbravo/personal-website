a21ae5aec61207933d4346596090a300941aacc2
Noah Bravo <36118057+noahbravo@users.noreply.github.com>
2025-10-08 15:57:12 +0200

    Update experience

--- app/utils/helpers/getExperience.ts
+++ app/utils/helpers/getExperience.ts
@@ -1,6 +1,6 @@
 export function getExperience() {
   const today = new Date()
-  const startDate = new Date('05/01/2015')
+  const startDate = new Date('01/05/2016')
   const diff = today.valueOf() - startDate.valueOf()
   const timeDiff = new Date(diff)
   return Math.abs(timeDiff.getUTCFullYear() - 1970)
